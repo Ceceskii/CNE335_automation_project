61c518e768ef3ed97bb0b21a8bac5f003f751acf
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2023-03-11 17:20:28 -0800

    Update main

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-# This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
+# Celine Watcharaapakorn-Smith
+# Automate_Ping
+# CNE 335
+
+from Server import Server
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Celine Watcharaapakorn-Smith")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    server_ip = "3.138.106.53"
+    rsa_key = r"C:\Users\celin\Downloads\Celine_Launch_and_Connect.ppk"
+    my_serv = Server(server_ip, rsa_key)
     # TODO - Call Ping method and print the results
+    print(my_serv.ping())
+    ssh_result = my_serv
+    print(ssh_result)

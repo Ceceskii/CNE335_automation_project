60731ed213cc486a6fd9d75a7450f5e7129a30c4
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2023-03-11 17:20:56 -0800

    Update server

--- Server.py
+++ Server.py
@@ -1,10 +1,15 @@
+import os
+import paramiko
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
-    def __init__(self, server_ip):
+    def __init__(self, server_ip, rsa_key ):
         # TODO -
         self.server_ip = server_ip
+        self.rsa_key = rsa_key
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        result = os.system("ping -n 5 %s" % self.server_ip)
+        return result
